2c2cf3486356d0d6a667a95c93e439cd6fe412bf
Suresh G <sureshg.g@gmail.com>
2020-08-15 04:53:30 -0700

    feat: added rsocket client support.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-## Dart2Native sample web server app.
+## Dart2Native HTTPS & [RSocket][rsocket] sample app
 
- `AOT` compiles a sample `Dart` web server to native executable for `Windows`, `Linux` and `macOS` using [Dart2Native][dart2native_url] tool.
+ `AOT` compiles a `Dart` app to native executable for `Windows`, `Linux` and `macOS` using [Dart2Native][dart2native_url] tool.
 
 [![GitHub Workflow Status][shieldio_img]][gha_url] 
+[![Dart][dart_img]][dart_url]
+[![Pub Version][doc_img]][doc_url]
 [![Style guide][sty_img]][sty_url]
-
 
 #### Build
 
@@ -38,15 +39,23 @@ $ openssl req -newkey rsa:4096 \
 
 #### Misc
 
+ * https://hub.docker.com/r/google/dart-runtime
  * https://github.com/cedx/setup-dart
  * https://github.com/DanTup/gh-actions
  * https://github.com/DanTup/dart-native-executables
  * https://tio.run/#powershell
-
+ 
+[rsocket]: https://rsocket.io
 [dart2native_url]: https://dart.dev/tools/dart2native
 
 [sty_url]: https://pub.dev/packages/pedantic
-[sty_img]: https://img.shields.io/badge/style-pedantic-40c4ff.svg?style=for-the-badge&logo=Dart&logoColor=40c4ff
+[sty_img]: https://img.shields.io/badge/style-pedantic-26C6DA.svg?style=for-the-badge&logo=Dart&logoColor=26C6DA
+
+[dart_url]: https://dart.dev/get-dart#release-channels
+[dart_img]: https://img.shields.io/docker/v/google/dart?color=40c4ff&label=Dart&logo=dart&logoColor=40c4ff&style=for-the-badge
+
+[doc_url]: https://pub.dev/documentation/rsocket/latest/
+[doc_img]: https://img.shields.io/pub/v/rsocket?color=orange&label=rsocket&logo=dart&logoColor=orange&style=for-the-badge
 
 [gha_url]: https://github.com/sureshg/dart-native-sample/actions
 [gha_img]: https://github.com/sureshg/dart-native-sample/workflows/Dart%20Build/badge.svg?branch=master
--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,8 @@ import 'package:dart_native_sample/utils.dart' as cert_util;
 import 'package:http/io_client.dart';
 import 'package:path/path.dart';
 import 'package:puppeteer/puppeteer.dart';
+import 'package:rsocket/payload.dart';
+import 'package:rsocket/rsocket_connector.dart';
 
 void main(List<String> args) async {
   var parser = ArgParser();
@@ -44,6 +46,9 @@ void main(List<String> args) async {
     print('Registering the Signal handlers...');
     registerSignalHandler();
   }
+
+  // rsocket client test
+  await rsocketDemo();
 
   // Read the certs and key
   String cert, key;
@@ -112,8 +117,8 @@ void printEnv() {
   print('''
 Runtime Env:
  OS: ${Platform.operatingSystem}: ${Platform.operatingSystemVersion}
-   Dart: ${Platform.version}
-  ''');
+ Dart: ${Platform.version}
+''');
 }
 
 /// Print the server cert details
@@ -123,7 +128,7 @@ void printCertDetails(HttpClientResponse res) {
   print('Server Cert SAN: ${certData.subjectAlternativNames}');
 }
 
-// Take the screenshot of a url using puppeteer.
+/// Take the screenshot of a url using puppeteer.
 void takeScreenShotOf(Uri uri) async {
   print('Getting the screenshot of ${uri}');
   // Start the browser and go to a web page
@@ -138,6 +143,24 @@ void takeScreenShotOf(Uri uri) async {
   // Save it to a file
   await File('${uri.host}.png').writeAsBytes(screenshot);
   await browser.close();
+}
+
+/// rsocket websocket transport demo.
+void rsocketDemo() async {
+  var rsocketUrl = 'wss://rsocket-demo.herokuapp.com/rsocket';
+  print('Connecting to rsocket url: $rsocketUrl');
+  var client = await RSocketConnector.create().connect(rsocketUrl);
+
+  print('Start streaming the data...');
+  var stream = await client.requestStream(Payload.fromText('', 'hello'));
+
+  var i = 1;
+  await stream.take(25).forEach((e) {
+    print('${i++} - ${e.getDataUtf8()}');
+  });
+
+  print('Closing the rsocket connection.');
+  client.close();
 }
 
 /// Handle all Http requests
